9959e94ee0e5dd9b2d15886dbd5fb0b13b24291a
shiv kumar <shiv79029@gmail.com>
2026-03-20 12:51:50 +0530

    Add login feature

---
+++ vulnerable.py
@@ -0,0 +1,14 @@
+import os
+import sqlite3
+
+def login(username, password):
+    # SQL Injection vulnerability
+    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+    conn = sqlite3.connect('db.sqlite')
+    cursor = conn.execute(query)
+    return cursor.fetchone()
+
+def run_command(user_input):
+    # Command injection vulnerability  
+    os.system(f"echo {user_input}")
+
